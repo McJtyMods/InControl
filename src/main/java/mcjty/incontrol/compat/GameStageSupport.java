5653839f356c74db7cf696d25c69a6353107ee1b
McJty <mcjty1@gmail.com>
2018-10-01 12:55:49 +0200

    Updated for the latest GameStages api (2.0)

--- src/main/java/mcjty/incontrol/compat/GameStageSupport.java
+++ src/main/java/mcjty/incontrol/compat/GameStageSupport.java
@@ -1,6 +1,7 @@
 package mcjty.incontrol.compat;
 
-import net.darkhax.gamestages.capabilities.PlayerDataHandler;
+import net.darkhax.gamestages.data.GameStageSaveHandler;
+import net.darkhax.gamestages.data.IStageData;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
@@ -10,8 +11,8 @@ public class GameStageSupport {
     public static boolean hasGameStage(DamageSource source, String stage) {
         Entity entity = source.getTrueSource();
         if (entity instanceof EntityPlayer) {
-            PlayerDataHandler.IStageData stageData = PlayerDataHandler.getStageData((EntityPlayer) entity);
-            return stageData.hasUnlockedStage(stage);
+            IStageData stageData = GameStageSaveHandler.getPlayerData(entity.getUniqueID());
+            return stageData.hasStage(stage);
         } else {
             return false;
         }
