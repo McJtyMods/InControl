87ad54e0d41287feb208aa266ad761b6d236fc44
McJty <mcjty1@gmail.com>
2019-01-13 09:33:08 +0100

    Version 3.8.3

--- src/main/java/mcjty/incontrol/InControl.java
+++ src/main/java/mcjty/incontrol/InControl.java
@@ -20,7 +20,7 @@ public class InControl {
 
     public static final String MODID = "incontrol";
     public static final String MODNAME = "InControl";
-    public static final String VERSION = "3.8.2";
+    public static final String VERSION = "3.8.3";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
 
     @SidedProxy(clientSide = "mcjty.incontrol.proxy.ClientProxy", serverSide = "mcjty.incontrol.proxy.ServerProxy")
