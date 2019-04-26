3a54f6176a04809bb80785b651512880e999db38
McJty <mcjty1@gmail.com>
2019-04-26 15:24:43 +0200

    Version bumps

--- src/main/java/mcjty/incontrol/InControl.java
+++ src/main/java/mcjty/incontrol/InControl.java
@@ -20,7 +20,7 @@ public class InControl {
 
     public static final String MODID = "incontrol";
     public static final String MODNAME = "InControl";
-    public static final String VERSION = "3.9.6";
+    public static final String VERSION = "3.9.7";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
 
     @SidedProxy(clientSide = "mcjty.incontrol.setup.ClientProxy", serverSide = "mcjty.incontrol.setup.ServerProxy")
