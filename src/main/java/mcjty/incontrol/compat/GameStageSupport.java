74e33f613a3bfeb0455222d7f4adcc970f996223
McJty <mcjty1@gmail.com>
2021-02-10 05:39:14 +0100

    - Gamestage support is working again

--- src/main/java/mcjty/incontrol/compat/GameStageSupport.java
+++ src/main/java/mcjty/incontrol/compat/GameStageSupport.java
@@ -1,33 +1,31 @@
 package mcjty.incontrol.compat;
 
+import net.darkhax.gamestages.GameStageHelper;
+import net.darkhax.gamestages.data.IStageData;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class GameStageSupport {
 
     public static boolean hasGameStage(PlayerEntity player, String stage) {
-        // @todo 1.15
-//        if (player != null) {
-//            IStageData stageData = GameStageHelper.getPlayerData(player);
-//            return stageData.hasStage(stage);
-//        } else {
-//            return false;
-//        }
-        return false;
+        if (player != null) {
+            IStageData stageData = GameStageHelper.getPlayerData(player);
+            return stageData.hasStage(stage);
+        } else {
+            return false;
+        }
     }
 
     public static void addGameStage(PlayerEntity player, String stage) {
-        // @todo 1.15
-//        if (player != null) {
-//            IStageData stageData = GameStageHelper.getPlayerData(player);
-//            stageData.addStage(stage);
-//        }
+        if (player != null) {
+            IStageData stageData = GameStageHelper.getPlayerData(player);
+            stageData.addStage(stage);
+        }
     }
 
     public static void removeGameStage(PlayerEntity player, String stage) {
-        // @todo 1.15
-//        if (player != null) {
-//            IStageData stageData = GameStageHelper.getPlayerData(player);
-//            stageData.addStage(stage);
-//        }
+        if (player != null) {
+            IStageData stageData = GameStageHelper.getPlayerData(player);
+            stageData.addStage(stage);
+        }
     }
 }
