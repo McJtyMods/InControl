d2d52bfc1b000221145010c119648a4c8762aaf1
McJty <mcjty1@gmail.com>
2023-04-25 15:49:05 +0200

    - Added support for the 'tag' keyword in a block description (in favor of the old and non functional 'ore' keyword)

--- src/main/java/mcjty/incontrol/tools/rules/RuleBase.java
+++ src/main/java/mcjty/incontrol/tools/rules/RuleBase.java
@@ -9,6 +9,8 @@ import mcjty.incontrol.ErrorHandler;
 import mcjty.incontrol.tools.typed.AttributeMap;
 import mcjty.incontrol.tools.varia.LookAtTools;
 import mcjty.incontrol.tools.varia.Tools;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
@@ -18,6 +20,8 @@ import net.minecraft.nbt.TagParser;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
@@ -40,6 +44,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.server.ServerLifecycleHooks;
 import org.apache.commons.lang3.StringUtils;
@@ -47,10 +53,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -168,12 +171,35 @@ public class RuleBase<T extends RuleBase.EventGetter> {
         });
     }
 
+    private static final Component DEFAULT_NAME = Component.literal("@");
+    private static final CommandSource EMPTY = new CommandSource() {
+        @Override
+        public void sendSystemMessage(Component component) {
+        }
+
+        @Override
+        public boolean acceptsSuccess() {
+            return false;
+        }
+
+        @Override
+        public boolean acceptsFailure() {
+            return false;
+        }
+
+        @Override
+        public boolean shouldInformAdmins() {
+            return false;
+        }
+    };
+
     private void addCommandAction(String command) {
         actions.add(event -> {
-            // @todo 1.15 new command system
-//            MinecraftServer server = event.getWorld().getServer();
-//            PlayerEntity player = event.getPlayer();
-//            server.commandManager.executeCommand(player != null ? player : new DummyCommandSender(event.getWorld(), null), command);
+            MinecraftServer server = event.getWorld().getServer();
+            Player player = event.getPlayer();
+            CommandSourceStack stack = new CommandSourceStack(EMPTY, Vec3.atCenterOf(event.getPosition()), Vec2.ZERO, (ServerLevel) event.getWorld(), 2,
+                    DEFAULT_NAME.getString(), DEFAULT_NAME, server, player);
+            server.getCommands().performPrefixedCommand(stack, command);
         });
     }
 
@@ -709,7 +735,6 @@ public class RuleBase<T extends RuleBase.EventGetter> {
                     if (entityLiving instanceof EnderMan) {
                         if (item.getItem() instanceof BlockItem) {
                             BlockItem b = (BlockItem) item.getItem();
-                            // @todo 1.15 metadata
                             ((EnderMan) entityLiving).setCarriedBlock(b.getBlock().defaultBlockState());
                         }
                     } else {
@@ -726,7 +751,6 @@ public class RuleBase<T extends RuleBase.EventGetter> {
                     if (entityLiving instanceof EnderMan) {
                         if (item.getItem() instanceof BlockItem) {
                             BlockItem b = (BlockItem) item.getItem();
-                            // @todo 1.15 metadata
                             ((EnderMan) entityLiving).setCarriedBlock(b.getBlock().defaultBlockState());
                         }
                     } else {
