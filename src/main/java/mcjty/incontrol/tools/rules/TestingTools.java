98c89e14864c47f372c576b8f3872049bf597aa3
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2024-04-30 10:56:12 +0200

    - The 'mod' test in item filters was not working at all - Item filters have improved. It's now possible to not have to specify the item but instead match on tag or mod only

--- src/main/java/mcjty/incontrol/tools/rules/TestingTools.java
+++ src/main/java/mcjty/incontrol/tools/rules/TestingTools.java
@@ -179,11 +179,16 @@ public class TestingTools {
             return s -> s.isEmpty() == empty;
         }
 
-        String name = obj.get("item").getAsString();
-        Item item = BuiltInRegistries.ITEM.get(ResourceLocation.parse(name));
-        if (item == null) {
-            ErrorHandler.error("Unknown item '" + name + "'!");
-            return null;
+        Item item;
+        if (obj.has("item")) {
+            String name = obj.get("item").getAsString();
+            item = BuiltInRegistries.ITEM.get(ResourceLocation.parse(name));
+            if (item == null) {
+                ErrorHandler.error("Unknown item '" + name + "'!");
+                return null;
+            }
+        } else {
+            item = null;
         }
 
         Predicate<ItemStack> test;
@@ -192,50 +197,80 @@ public class TestingTools {
             if (damage == null) {
                 return null;
             }
-            test = s -> s.getItem() == item && damage.test(s.getDamageValue());
+            if (item == null) {
+                test = s -> damage.test(s.getDamageValue());
+            } else {
+                test = s -> s.getItem() == item && damage.test(s.getDamageValue());
+            }
         } else {
-            test = s -> s.getItem() == item;
+            if (item == null) {
+                test = null;
+            } else {
+                test = s -> s.getItem() == item;
+            }
         }
 
         if (obj.has("count")) {
             Predicate<Integer> count = getExpression(obj.get("count"));
             if (count != null) {
-                Predicate<ItemStack> finalTest = test;
-                test = s -> finalTest.test(s) && count.test(s.getCount());
+                if (test == null) {
+                    test = s -> count.test(s.getCount());
+                } else {
+                    Predicate<ItemStack> finalTest = test;
+                    test = s -> finalTest.test(s) && count.test(s.getCount());
+                }
             }
         }
         if (obj.has("tag")) {
             ResourceLocation tagname = ResourceLocation.parse(obj.get("tag").getAsString());
             TagKey<Item> key = TagKey.create(Registries.ITEM, tagname);
-            Predicate<ItemStack> finalTest = test;
-            test = s -> finalTest.test(s) && s.is(key);
+            if (test == null) {
+                test = s -> s.is(key);
+            } else {
+                Predicate<ItemStack> finalTest = test;
+                test = s -> finalTest.test(s) && s.is(key);
+            }
         }
         if (obj.has("mod")) {
             String mod = obj.get("mod").getAsString();
-            Predicate<ItemStack> finalTest = test;
-            test = s -> finalTest.test(s) && "mod".equals(BuiltInRegistries.ITEM.getKey(s.getItem()).getNamespace());
+            if (test == null) {
+                test = s -> mod.equals(BuiltInRegistries.ITEM.getKey(s.getItem()).getNamespace());
+            } else {
+                Predicate<ItemStack> finalTest = test;
+                test = s -> finalTest.test(s) && mod.equals(BuiltInRegistries.ITEM.getKey(s.getItem()).getNamespace());
+            }
         }
         if (obj.has("nbt")) {
             List<Predicate<CompoundTag>> nbtMatchers = getNbtMatchers(obj);
             if (nbtMatchers != null) {
-                Predicate<ItemStack> finalTest = test;
-                test = s -> finalTest.test(s) && nbtMatchers.stream().allMatch(p -> {
                     // @todo 1.21 better system!
-                    DataComponentPatch patch = s.getComponentsPatch();
-                    DataResult<Tag> result = DataComponentPatch.CODEC.encodeStart(NbtOps.INSTANCE, patch);
-                    Tag tag = result.getOrThrow();
-                    return p.test((CompoundTag) tag);
-                });
+		    xxx
+                    //DataComponentPatch patch = s.getComponentsPatch();
+                    //DataResult<Tag> result = DataComponentPatch.CODEC.encodeStart(NbtOps.INSTANCE, patch);
+                    //Tag tag = result.getOrThrow();
+                if (test == null) {
+                    test = s -> nbtMatchers.stream().allMatch(p -> p.test(s.getTag()));
+                } else {
+                    Predicate<ItemStack> finalTest = test;
+                    test = s -> finalTest.test(s) && nbtMatchers.stream().allMatch(p -> p.test(s.getTag()));
+                }
             }
         }
         if (obj.has("energy")) {
             Predicate<Integer> energy = getExpression(obj.get("energy"));
             if (energy != null) {
-                Predicate<ItemStack> finalTest = test;
-                test = s -> finalTest.test(s) && energy.test(getEnergy(s));
+                if (test == null) {
+                    test = s -> energy.test(getEnergy(s));
+                } else {
+                    Predicate<ItemStack> finalTest = test;
+                    test = s -> finalTest.test(s) && energy.test(getEnergy(s));
+                }
             }
         }
 
+        if (test == null) {
+            ErrorHandler.error("No item description found!");
+        }
         return test;
     }
 
