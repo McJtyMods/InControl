d2596d4bc5cd4a05ce58b954945ff8f748808bcd
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2025-11-28 18:08:03 +0100

    Various fixes

--- src/main/java/mcjty/incontrol/compat/CustomNPCSupport.java
+++ src/main/java/mcjty/incontrol/compat/CustomNPCSupport.java
@@ -1,37 +1,36 @@
 package mcjty.incontrol.compat;
 
+import mcjty.incontrol.ErrorHandler;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import noppes.npcs.CustomEntities;
+import noppes.npcs.api.NpcAPI;
+import noppes.npcs.entity.EntityNPCInterface;
 
 public class CustomNPCSupport {
 
     public static Entity getNpcEntity(ServerLevel level, int cloneTab, String cloneName) {
-        // @todo 1.21
-//        try {
-//            Entity entity = NpcAPI.Instance().getClones().get(cloneTab, cloneName, NpcAPI.Instance().getIWorld(level)).getMCEntity();
-//            entity.getPersistentData().putInt("InControlNatSpawnTab", cloneTab);
-//            entity.getPersistentData().putString("InControlNatSpawnName", cloneName);
-//            return entity;
-//        } catch (Exception e) {
-//            ErrorHandler.error("Error spawning NPC (" + e.getMessage() + ")");
-//            return null;
-//        }
-        return null;
+        try {
+            Entity entity = NpcAPI.Instance().getClones().get(cloneTab, cloneName, NpcAPI.Instance().getIWorld(level)).getMCEntity();
+            entity.getPersistentData().putInt("InControlNatSpawnTab", cloneTab);
+            entity.getPersistentData().putString("InControlNatSpawnName", cloneName);
+            return entity;
+        } catch (Exception e) {
+            ErrorHandler.error("Error spawning NPC (" + e.getMessage() + ")");
+            return null;
+        }
     }
 
     public static EntityType<?> getNPCEntityType() {
-//        return CustomEntities.entityCustomNpc;
-        return null;
+        return CustomEntities.entityCustomNpc;
     }
 
     public static boolean isNPC(Entity entity) {
-//        return entity.getType() == CustomEntities.entityCustomNpc;
-        return false;
+        return entity.getType() == CustomEntities.entityCustomNpc;
     }
 
     public static boolean hasNPCInterface(Entity entity) {
-//        return entity instanceof EntityNPCInterface;
-        return false;
+        return entity instanceof EntityNPCInterface;
     }
 }
